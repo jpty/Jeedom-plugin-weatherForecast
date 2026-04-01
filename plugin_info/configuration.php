78ba1a5f52e2788f83fe54b5c8f9423e5f698250
jpty <1798636+jpty@users.noreply.github.com>
2026-04-01 19:35:23 +0200

    Add files via upload

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -24,19 +24,53 @@ if (!isConnect('admin')) {
 <form class="form-horizontal">
 	<fieldset>
     <div class="form-group">
-      <label class="col-lg-4 control-label">{{Clef API OpenWeatherMap}}</label>
+      <label class="col-md-3 control-label">{{Clef API OpenWeatherMap}}</label>
       <div class="col-lg-4">
         <input class="configKey form-control" data-l1key="apikeyOwm" />
       </div>
       <a href="https://home.openweathermap.org/api_keys" target="_blank">Site Web OpenWeather ICI</a> <sup><i class="fas fa-question-circle tooltips" title="{{La clé API est à récupérer dans votre compte sur le site openweathermap.org<br/>Aprés génération d'une clé API, son activation n'est pas immédiate. Veuillez patienter.}}"></i></sup>
     </div>
     <div class="form-group">
-      <label class="col-lg-4 control-label">{{Clef API WeatherApi}}</label>
+      <label class="col-md-3 control-label">{{Clef API WeatherApi}}</label>
       <div class="col-lg-4">
         <input class="configKey form-control" data-l1key="apikeyWapi" />
       </div>
       <a href="https://www.weatherapi.com/my/" target="_blank">Site Web WeatherAPI ICI</a> <sup><i class="fas fa-question-circle tooltips" title="{{La clé API est à récupérer dans votre compte sur le site WeatherApi}}"></i></sup>
     </div>
+    <div class="form-group">
+      <label class="col-md-3 control-label">{{APPLICATION_ID Météo France}}
+        <a target="blank" href="https://portail-api.meteofrance.fr/web/fr/faq">Plus d'info ICI</a>
+        <sup><i class="fas fa-question-circle tooltips" title="{{A copier sur le site MF et à coller ici}}"></i></sup>
+      </label>
+      <div class="col-md-7">
+        <input type="text" class="configKey form-control" data-l1key="credentialApiMeteoFrance"/>
+      </div>
+    </div>
+    <div class="form-group">
+			<label class="col-md-3 control-label">{{Utilisation de l'API vigilance Météo France}}</label>
+			<div class="col-sm-1" style="width:2%">
+				<input type="checkbox" class="configKey tooltips" data-l1key="useVigilanceAPI">
+			</div>
+      <label class="col-md-6 control-label" style="text-align: left;line-height:normal">{{Le plugin peut récupérer les données de vigilances avec l'API <a href="https://portail-api.meteofrance.fr/web/fr/api/DonneesPubliquesVigilance">DonneesPubliquesVigilance</a> de Météo France ou sur le <a href="http://storage.gra.cloud.ovh.net/v1/AUTH_555bdc85997f4552914346d4550c421e/gra-vigi6-archive_public/">site d'archives de Météo France</a>.}}
+      </label>
+    </div>
+    <div class="form-group">
+			<label class="col-md-3 control-label">{{Utilisation de l'API Météo des forêts}}</label>
+			<div class="col-sm-1" style="width:2%">
+				<input type="checkbox" class="configKey tooltips" data-l1key="useForestAPI">
+			</div>
+      <label class="col-md-6 control-label" style="text-align: left;line-height:normal">{{Vous devez vous abonnez à l'API <a href="https://portail-api.meteofrance.fr/web/fr/api/DonneesPubliquesMeteoForets">DonneesPubliquesMeteoForets</a> de Météo France.</a>.}}
+      </label>
+		</div>
+<!--
+    <div class="form-group">
+      <label class="col-md-3 control-label">{{Commentaires}}
+      </label>
+      <div class="col-md-7">
+        <input type="text" class="configKey form-control" data-l1key="comment"/>
+      </div>
+    </div>
+-->
   </fieldset>
 </form>
 
